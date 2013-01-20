d314a4da4ba2c9cc70b07f492635c65f37c1b947
higumachan <harekumo792154@gmail.com>
2013-01-20 16:31:31 +0900

    initial commit

---
+++ ex_test.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+from nose.tools import ok_, eq_
+import ex
+
+def add_test():
+    eq_(ex.add(1, 2), 3);
+    eq_(ex.add(1, 5), 6);
+
+def sub_test():
+    eq_(ex.sub(1, 1), 0);
+    eq_(ex.sub(2, 1), 1);
+    eq_(ex.sub(1, 2), -1);
+
+
